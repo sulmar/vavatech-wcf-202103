f40ab0dd67303c89496937398aecd538f506eb76
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-23 11:15:56 +0100

    Message Contracts

---
+++ 05_MessageContracts/EmployeeService/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeServices
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public EmployeeResponse Get(EmployeeRequest request)
+        {
+            EmployeeResponse response = new Faker<EmployeeResponse>()
+                .RuleFor(p=>p.Id, f=>request.EmployeeId)
+                .RuleFor(p=>p.FirstName, f=>f.Person.FirstName)
+                .RuleFor(p => p.LastName, f => f.Person.LastName)
+                .RuleFor(p=>p.Version, f=>"v1")
+                .Generate();
+
+            return response;
+            
+        }
+    }
+}
---
+++ 05_MessageContracts/EmployeeService/IEmployeeService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeServices
+{
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        EmployeeResponse Get(EmployeeRequest request);
+    }
+
+    [MessageContract]
+    public class EmployeeRequest
+    {
+        [MessageHeader(Namespace = "http://vavatech.pl/")]
+        public string LicenseKey { get; set; }
+
+        [MessageBodyMember]
+        public int EmployeeId { get; set; }
+    }
+
+    [MessageContract(WrapperNamespace = "http://vavatech.pl/EmployeeResponse")]
+    public class EmployeeResponse
+    {
+        [MessageBodyMember(Order = 1)]
+        public int Id { get; set; }
+        [MessageBodyMember(Order = 2)] 
+        public string FirstName { get; set; }
+        [MessageBodyMember(Order = 3)] 
+        public string LastName { get; set; }
+        [MessageHeader]
+        public string Version { get; set; }
+    }
+
+
+}
