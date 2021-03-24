10ff7b29793c5c02d8f1e64a4ecc2da4f5440ada
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-24 16:01:48 +0100

    Windows Service

--- 08_DotNetCoreApi/WebApiService/Program.cs
+++ 08_DotNetCoreApi/WebApiService/Program.cs
@@ -16,11 +16,29 @@ namespace WebApiService
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Running Our Web App As A Service
+        // https://dotnetcoretutorials.com/2019/12/21/hosting-an-asp-net-core-web-app-as-a-windows-service-in-net-core-3/
+        // Install-Package Microsoft.Extensions.Hosting.WindowsServices
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.UseUrls("https://*:5010");
+                })
+                .UseWindowsService()   // <- Windows Service
+            ;
+
+
+        // Publikacja aplikacji
+        // dotnet publish -r win-x64 -c Release
+
+        // Utworzenie us³ugi
+        // sc create TestService BinPath="C:\...\win-x64\publish\WebApiService.exe"
+        // sc start TestService
+        // sc stop TestService
+        // sc delete TestService
+
+
     }
 }
